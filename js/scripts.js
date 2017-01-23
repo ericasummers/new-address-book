93cdf3eac1e650be42a38df084979c9812b1a87d
Erica Wright and Koji Nakagawa <koji@>
2017-01-23 08:42:31 -0800

    add link to click on contact to see full first and last name

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,13 @@ $(document).ready(function() {
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".firstname").text(newContact.firstName);
+      $(".lastname").text(newContact.lastName);
+    });
+
     $("input#firstname").val("");
     $("input#lastname").val("");
 
